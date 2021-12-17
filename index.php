436ca950a25fd70f9328a2e9c99e0ac8fe31a5f8
robzenhu <rzenhu@gmail.com>
2021-12-17 20:42:33 -0300

    Formatando estilo

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Calculo de Consumo de Combustivel</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
 </head>  
 <body>
     <main>
